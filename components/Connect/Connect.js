7063a559c50b3944a242da59f4b30f0130cf6b91
pavel.and.ivanov <pavel.and.ivanov@megafon.ru>
2022-04-29 15:55:19 +0300

    Валидация

--- components/Connect/Connect.js
+++ components/Connect/Connect.js
@@ -4,17 +4,24 @@ import ArrowIcon from '../../public/svg/arrow_right.svg'
 import CloseIcon from '../../public/svg/close.svg'
 import { connect } from "react-redux";
 import { showModal } from "../../redux/slices/modalsSlice";
+import { Formik } from 'formik'
+import { string, object } from 'yup'
 
 
 function Connect( { isHidden, setIsHidden, showModal } ) {
+
+   const validationSchema = object().shape( {
+      phone: string().min( 16 ).required(),
+      name: string().min( 2 ).required(),
+   } )
 
    const isShow = !isHidden
       ? `${ s._ } d-block`
       : s._
 
-   function submitHandler(e) {
-      e.preventDefault()
+   function submitHandler(actions) {
       showModal( { modal: 'orderThx', bool: true } )
+      actions.resetForm()
    }
 
 
@@ -40,24 +47,56 @@ function Connect( { isHidden, setIsHidden, showModal } ) {
                Отправьте заявку, чтобы <nobr>подключить тариф</nobr>
             </p>
 
-            <form className={ s.form } onSubmit={ submitHandler }>
-               <div className={ s.inputs }>
-                  <CustomInput className={ s.input } name={ 'name' } placeholder={ 'Имя' }/>
-                  <CustomInput className={ s.input } name={ 'phone' } placeholder={ 'Номер телефона' }/>
-               </div>
+            <Formik
+               initialValues={ {
+                  name: '',
+                  phone: '',
+               } }
+               validateOnBlur
+               validationSchema={ validationSchema }
+               onSubmit={ (values, actions) => {
+                  submitHandler(actions)
+               }  }
+            >
+               { ( { values, errors, touched, dirty, handleChange, handleBlur, handleSubmit } ) =>
+                  <form className={ s.form } onSubmit={ handleSubmit }>
+                     <div className={ s.inputs }>
+                        <CustomInput
+                           className={ s.input }
+                           name={ 'name' }
+                           placeholder={ 'Имя' }
+                           value={ values.name }
+                           handleBlur={ handleBlur }
+                           handleChange={ handleChange }
+                           errors={ errors }
+                           touched={ touched }
+                           dirty={ dirty }
+                        />
+                        <CustomInput
+                           className={ s.input }
+                           name={ 'phone' }
+                           placeholder={ 'Номер телефона' }
+                           value={ values.phone }
+                           handleBlur={ handleBlur }
+                           handleChange={ handleChange }
+                           errors={ errors }
+                           touched={ touched }
+                           dirty={ dirty }
+                        />
+                     </div>
 
-               <p className={ s.consent }>
-                  Нажимая на кнопку, я даю <a href=' ' className={ `${ s.link } link` }>согласие</a> на обработку
-                  персональных данных и
-                  получение информации об услугах
-               </p>
-
-               <button type="submit" className={ s.btn }>Подключить <ArrowIcon className={ s.icon }/></button>
-            </form>
+                     <p className={ s.consent }>
+                        Нажимая на кнопку, я даю <a href=' ' className={ `${ s.link } link` }>согласие</a> на обработку
+                        персональных данных и
+                        получение информации об услугах
+                     </p>
+                     <button type="submit" className={ s.btn }>Подключить <ArrowIcon className={ s.icon }/></button>
+                  </form>
+               }
+            </Formik>
 
          </div>
       </div>
-
    )
 }
 
--- components/Connect/CustomInput/CustomInput.js
+++ components/Connect/CustomInput/CustomInput.js
@@ -1,21 +1,32 @@
 import s from './CustomInput.module.sass'
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import $ from "jquery";
 
 
 function CustomInput( props ) {
-
-   const [ value, setValue ] = useState( '' )
 
    useEffect( () => {
       window.mask = require( '../../../plugins/jquery.mask' )
       $( 'input[name="phone"]' ).mask( '+7(000)000-00-00' )
    }, [] )
 
+   function valid( errors, touch, dirty ) {
+      if ( errors && touch ) return s.error
+      if ( !errors && dirty ) return s.valid
+   }
+
+
    return (
       <label className={ `${ s._ } ${ props.className }` }>
-         <input type="text" name={ props.name } onInput={ ( e ) => setValue( e.target.value ) }/>
-         <span hidden={ value } className={ s.label }>{ props.placeholder }<span>*</span></span>
+         <input
+            type="text"
+            name={ props.name }
+            value={ props.value }
+            onInput={ props.handleChange }
+            onBlur={ props.handleBlur }
+            className={ valid( props.errors[props.name], props.touched[props.name], props.dirty ) }
+         />
+         <span hidden={ props.value } className={ s.label }>{ props.placeholder }<span>*</span></span>
       </label>
    )
 }
